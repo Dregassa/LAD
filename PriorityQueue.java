346d3029c6dd8754e250c355498dc9ebe057ba0e
aboblin <aboylan@stuy.edu>
2017-04-21 11:13:28 -0400

    added comments and generic typing

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,9 +1,16 @@
-public interface PriorityQueue{
+public interface PriorityQueue<T>{
 
-    public void add(Comparable item);
+    //add item to the end of the Queue
+    public void add(T item);
+    
+    //checks to see if the queue is empty, returns true if so
     public boolean isEmpty();
-    public Comparable peekMin();
-    public Comparable removeMin();
+    
+    //returns the highest priority item in the Queue
+    public T peekMin();
+    
+    //removes and the returns the highest priority item in the Queue
+    public T removeMin();
 
 
 }
