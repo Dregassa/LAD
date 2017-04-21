ce9a19d26e3cfd7e38a4b1e1cc0566200a8cb4b9
LMOlevsky <lolevsky@stuy.edu>
2017-04-21 11:42:34 -0400

    added mutators and accessors

---
+++ HelpDesk.java
@@ -0,0 +1,18 @@
+public class HelpDesk{
+
+    private ArrayPriorityQueue<Ticket> _queue;
+    private int _nextID;
+
+    HelpDesk(){
+	queue = new ArrayPriorityQueue<Ticket>();
+	_nextID = 0;
+    }
+
+
+    public static void main(String[] args){
+
+	
+
+    }
+
+}
