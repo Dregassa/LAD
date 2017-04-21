ee99cfb621e24b5ae08d4fba65ebcd601562697a
Dregassa <dregassa@stuy.edu>
2017-04-21 11:24:34 -0400

    Ticket added

---
+++ Ticket.java
@@ -0,0 +1,24 @@
+public class Ticket implements Comparable{
+
+    private int _id;
+    private int _VIPLevel;
+    private String _problem;
+    private Boolean _isSolved;
+    private String _name;
+    private String _solution;
+
+    public set
+
+    Ticket(String problem, int VIPLevel, String name){
+	
+    }
+
+    Ticket(){
+	return;
+    }
+
+    public Boolean compareTo(Ticket other){
+	
+    }
+
+}
